58cd19880ba7bbf2c53a280641d7d7c7d5d9ed77
Jae Hyeon Lee <jaekor91@gmail.com>
2017-12-20 17:38:15 -0600

    Script used to compute RAW vs Weighted densities of DEEP2 catalogs

---
+++ DEEP2-make-table.py
@@ -0,0 +1,27 @@
+from utils import *
+
+dir_derived = "../data/derived/"
+print "Import estimated areas"
+areas = np.load(dir_derived+"spec-area.npy")    
+
+print "Import DEEP2 combined catalogs"
+cns = []
+weights = []
+for fnum in [2, 3, 4]:
+    pcat = load_fits_table(dir_derived+"/deep2-f%d-photo-redz-oii.fits" % fnum)
+    cns.append(pcat["cn"].astype(int))
+    weights.append(pcat["TARG_WEIGHT"])
+
+table_header = generate_table_header()
+print(table_header)
+print "Raw number density"
+class_breakdown([2, 3, 4], cns, weights, areas, rwd="R")
+print "\n"
+
+print "Weighted number density"
+class_breakdown([2, 3, 4], cns, weights, areas, rwd="W")
+print "\n"
+
+print "The total densities must match."
+
+# print("Completed.\n")
