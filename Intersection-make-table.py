109f1b9dea6df2a2ae18ebecb5f50f6c43fca866
Jae Hyeon Lee <jaekor91@gmail.com>
2017-12-20 18:15:21 -0600

    Added an info about DR5 unmatched set fraction.

--- Intersection-make-table.py
+++ Intersection-make-table.py
@@ -7,10 +7,15 @@ areas = np.load(dir_derived+"spec-area.npy")
 print "Import Intersection catalogs"
 cns = []
 weights = []
-for fnum in [2, 3, 4]:
+print "Field: Unmatched fraction"
+for i, fnum in enumerate([2, 3, 4]):
     pcat = load_fits_table(dir_derived+"DR5-matched-to-DEEP2-f%d-glim24p25.fits" % fnum)
     cns.append(pcat["cn"].astype(int))
     weights.append(pcat["TARG_WEIGHT"])
+    num_unmatched = (pcat["DEEP2_matched"] == 0).sum()
+    print "F%d: %.2f%%" % (fnum, num_unmatched/float(cns[i].size) * 100)
+print "\n"
+
 
 table_header = generate_table_header()
 print "Raw number density"
