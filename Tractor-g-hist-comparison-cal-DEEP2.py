8b90939b95ce424325d7173bf3f8be37e94868aa
Jae Hyeon Lee <jaekor91@gmail.com>
2018-02-02 16:05:05 -0500

    Changed the figure fonts

--- Tractor-g-hist-comparison-cal-DEEP2.py
+++ Tractor-g-hist-comparison-cal-DEEP2.py
@@ -17,14 +17,14 @@ for fnum in range(2,5):
 
 
     # Plot 
-    fig, ax = plt.subplots(1, figsize=(7, 7))
+    fig, ax = plt.subplots(1, figsize=(5, 4))
     dm =0.05
     mag_bins = np.arange(19, 24+0.05, dm)
     ax.hist(g_cal, bins=mag_bins, color="black", histtype="step", label="DR5 Cal.", weights=np.ones(g_cal.size)/A, lw=2)
     ax.hist(g_DEEP2, bins=mag_bins, color="red", histtype="step", label="DEEP2 F%d"%fnum, weights=weights/area, lw=2)
     ax.set_xlim([19, 24])
-    ax.set_xlabel("g", fontsize=25)
-    ax.set_ylabel("dN/d(%.2fmag)" % dm, fontsize=25)
+    ax.set_xlabel("g", fontsize=20)
+    ax.set_ylabel("dN/d(%.2fmag)" % dm, fontsize=20)
     ax.legend(loc="upper left", fontsize=20)
     plt.savefig("../figures/g-hist-comparison-F%d.png" % fnum, dpi=400, bbox_inches="tight")
     # plt.show()
